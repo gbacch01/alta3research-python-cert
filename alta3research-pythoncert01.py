6ed94d140ddb3d62b061ca53e640b3e4340a4b9d
gbacch01 <83227810+gbacch01@users.noreply.github.com>
2021-05-25 21:14:10 -0400

    Add files via upload

---
+++ alta3research-pythoncert01.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 16:59:56 2021
+
+@author: Gibran  | The purpose of this script is to demonstrate proficiency with Python for an Alta3 certification.  
+Course started on April 26th with Chad Feeser.
+
+Make an API request to Https://api.nasa.gov and display the number of potentially hazardous asteroids, returns it in 
+an easily read format.
+"""
+
+#Import third party requests library rather than urllib.
+import requests
+
+def main():
+    #Create r which is our requests object.
+    r = requests.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY")
+
+ 
+    # the .json() method will dump a JSON string into Pythonic data structures. 
+    data = r.json()  #Convert Json to Pythonic dictionary
+
+    #Set initial hazards to zero
+    hazard= 0
+    #For loop iterates through the dictionary for each near earth object.
+    for date in data["near_earth_objects"]:
+        for dict in data["near_earth_objects"][date]:
+            #Keeps a tally of each occurrence of "is_potentially_hazardous_asteroid"
+            if "is_potentially_hazardous_asteroid":      
+                hazard += 1
+
+    #Print the total number of hazardous asteroids
+    print("The number of hazardous asteroids is: " + str(hazard))
+   
+
+main()
+
